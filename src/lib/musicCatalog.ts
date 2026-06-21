fe1c24dbc5a09a888e0f6ca9c59dab758b71b928
runningwolf-srk <runningwolf1987@gmail.com>
2026-06-21 02:00:34 -0500

    Create musicCatalog.ts

---
+++ src/lib/musicCatalog.ts
@@ -0,0 +1,20 @@
+export const musicCatalog = [
+  {
+    id: "HNYdZTp1qNc",
+    title: "Viking Short 1",
+    tag: "IRON COLLIDE",
+    cover:"/covers/iron-collide.jpg",
+  },
+  {
+    id: "UT1c5Q81kVk",
+    title: "Viking Short 2",
+    tag: "HORN OF WAR",
+    cover: "/covers/horn-of-war.jpg",
+  },
+  {
+    id: "NQTRyHDDlK0",
+    title: "Viking Short 3",
+    tag: "SPIRITUAL JOURNEY",
+    cover: "/covers/spiritual-journey.jpg",
+  },
+]
