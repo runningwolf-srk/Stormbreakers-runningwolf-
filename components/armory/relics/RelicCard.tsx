25b1e362099d952b1645ae19cccfd1a43b03b2ce
runningwolf-srk <runningwolf1987@gmail.com>
2026-07-05 18:15:53 -0500

    Refactor RelicCard to RelicGrid component

---
+++ components/armory/relics/RelicCard.tsx
@@ -0,0 +1,13 @@
+'use client';
+import { RELICS } from '@/data/relics';
+import { RelicCard } from './RelicCard';
+
+export function RelicGrid() {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+      {RELICS.map((relic) => (
+        <RelicCard key={relic.id} relic={relic} onClick={() => {}} />
+      ))}
+    </div>
+  );
+}
