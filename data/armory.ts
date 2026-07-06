2c6d1f878f93906c1c82a4325e02de82f0fc7f86
runningwolf-srk <runningwolf1987@gmail.com>
2026-07-06 16:36:23 -0500

    Refactor Relic type and update armory data
    Updated the Relic type structure and modified the armory data entries to include new fields and values.

--- data/armory.ts
+++ data/armory.ts
@@ -1,75 +1,58 @@
-export type Relic = {  // ✅ MUST HAVE 'export' HERE
-  id: string;
-  slug: string;
-  name: string;
+// data/armory.ts
+export type Relic = {
+  relic: string;
+  song: string;
   image: string;
-  songTitle: string;
-  youtubeUrl: string;
+  youtube: string;
 };
 
-export const RELICS: Relic[] = [
+export const armory: Relic[] = [
   {
-    id: "storm-crown",
-    slug: "storm-crown",
-    name: "Storm Crown",
-    image: "/relics/storm-crown.jpg",
-    songTitle: "Jesus is an healer",
-    youtubeUrl: "https://youtu.be/stsryByK5h8", // REPLACE WITH YOUR LINK
+    relic: "Storm Crown",
+    song: "Jesus is an healer",
+    image: "/file_00000000fa471f882fd16e52afa59fd.png",
+    youtube: "https://youtu.be/stsryByK5h8?is=DDb3dph6s9-Ee9xe"
   },
   {
-    id: "eternal-fire",
-    slug: "eternal-fire", 
-    name: "Eternal Fire",
-    image: "/relics/eternal-fire.jpg",
-    songTitle: "Lord of Lords",
-    youtubeUrl: "https://youtu.be/9vP0NPrEv9s", // REPLACE WITH YOUR LINK
+    relic: "I'm on fire",
+    song: "I'm on fire",
+    image: "/file_0000000065a071f5832301f52d11fb80.png",
+    youtube: "https://youtu.be/8XQUhWB_N5M?is=s4N5xkTv3xRZc3Nh"
   },
   {
-    id: "first-flame",
-    slug: "first-flame",
-    name: "The First Flame", 
-    image: "/relics/first-flame.jpg",
-    songTitle: "Spiritual Journey",
-    youtubeUrl: "https://youtu.be/umDFjJjh0_c", // REPLACE WITH YOUR LINK
+    relic: "Horn of war",
+    song: "Horn of war",
+    image: "/f9a9d930-631f-11f1-94f7-f3f3b6c0f03c.webp",
+    youtube: "https://youtu.be/M4wGCg5oCx0?is=tYA8M-DSzY_cvjy1"
   },
   {
-    id: "wolf-fang",
-    slug: "wolf-fang",
-    name: "Wolf Fang",
-    image: "/relics/wolf-fang.jpg",
-    songTitle: "I'm on fire",
-    youtubeUrl: "https://youtu.be/8XQUhWB_N5M", // REPLACE WITH YOUR LINK
+    relic: "Heaven's Key", // UPDATE THIS
+    song: "Heaven Calling",
+    image: "/29ed31f0-6320-11f1-94f7-f3f3b6c0f03c.webp",
+    youtube: "https://youtu.be/oxNauKuxg4Q?is=33T6e9y3JhI4MFMd"
   },
   {
-    id: "thunder-drums",
-    slug: "thunder-drums",
-    name: "Thunder Drums",
-    image: "/relics/thunder-drums.jpg", 
-    songTitle: "Horn of War",
-    youtubeUrl: "https://youtu.be/M4wGCg5oCx0", // REPLACE WITH YOUR LINK
+    relic: "Spirit Mantle", // UPDATE THIS
+    song: "Iron Collide",
+    image: "/1fe52410-6320-11f1-94f7-f3f3b6c0f03c.webp",
+    youtube: "https://youtu.be/fIkUDO2emoc?is=h6CFtJjuQpclL-Sx"
   },
   {
-    id: "spirit-mantle",
-    slug: "spirit-mantle",
-    name: "Spirit Mantle",
-    image: "/relics/spirit-mantle.jpg",
-    songTitle: "Iron Collide", 
-    youtubeUrl: "https://youtu.be/fIkUDO2emoc", // REPLACE WITH YOUR LINK
+    relic: "Bloodline Seal", // UPDATE THIS
+    song: "Blood of Cross",
+    image: "/148e9d30-6320-11f1-94f7-f3f3b6c0f03c.webp",
+    youtube: "https://youtu.be/4lcbjsNLlzo?is=VrYf7tD6KR163MAw"
   },
   {
-    id: "chronicle-stone",
-    slug: "chronicle-stone",
-    name: "Chronicle Stone",
-    image: "/relics/chronicle-stone.jpg",
-    songTitle: "Heaven Calling",
-    youtubeUrl: "https://youtu.be/oxNauKuxg4Q", // REPLACE WITH YOUR LINK
+    relic: "King's Mantle", // UPDATE THIS
+    song: "Lord of Lords",
+    image: "/060a2ef0-6320-11f1-94f7-f3f3b6c0f03c.webp",
+    youtube: "https://youtu.be/9vP0NPrEv9s?is=2fTqYITlEiIK-8jg"
   },
   {
-    id: "runningwolfs-blade",
-    slug: "runningwolfs-blade", 
-    name: "Runningwolf's Blade",
-    image: "/relics/runningwolfs-blade.jpg",
-    songTitle: "8TH SONG TITLE HERE", // ← REPLACE ME
-    youtubeUrl: "8TH YOUTUBE LINK HERE", // ← REPLACE ME
-  },
+    relic: "Spirit Walker", // UPDATE THIS
+    song: "Spiritual Journey",
+    image: "/e8a21b70-631f-11f1-94f7-f3f3b6c0f03c.webp",
+    youtube: "https://youtu.be/umDFjJjh0_c?is=X8ig4Cu59jjFPHvF"
+  }
 ];
