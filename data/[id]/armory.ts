66c03cefceb0cebc6bd7a040c5a9ae5117058dcc
runningwolf-srk <runningwolf1987@gmail.com>
2026-07-06 13:13:17 -0500

    Define Relic type and add new relics
    Added a new type 'Relic' and updated RELICS array with new relic entries including IDs.

--- data/[id]/armory.ts
+++ data/[id]/armory.ts
@@ -1,15 +1,75 @@
-import { getRelicById } from '@/data/[id]/armory';
+export type Relic = {
+  id: string; // ADD THIS LINE
+  slug: string;
+  name: string;
+  image: string;
+  songTitle: string;
+  youtubeUrl: string;
+};
 
-export default function RelicPage({ params }: { params: { id: string } }) {
-  const relic = getRelicById(params.id);
-  
-  if (!relic) return <div>Relic not found</div>;
-  
-  return (
-    <div key={relic.id}>
-      <h1>{relic.name}</h1>
-      <img src={relic.image} alt={relic.name} />
-      <a href={relic.youtubeUrl}>{relic.songTitle}</a>
-    </div>
-  );
-}
+export const RELICS: Relic[] = [
+  {
+    id: "storm-crown", // ADD THIS LINE
+    slug: "storm-crown",
+    name: "Storm Crown", 
+    image: "/relics/storm-crown.jpg",
+    songTitle: "Jesus is an healer",
+    youtubeUrl: "https://youtu.be/stsryByK5h8",
+  },
+  {
+    id: "eternal-fire", // ADD THIS LINE
+    slug: "eternal-fire",
+    name: "Eternal Fire",
+    image: "/relics/eternal-fire.jpg", 
+    songTitle: "Lord of Lords",
+    youtubeUrl: "https://youtu.be/9vP0NPrEv9s",
+  },
+  {
+    id: "first-flame",
+    slug: "first-flame",
+    name: "The First Flame",
+    image: "/relics/first-flame.jpg",
+    songTitle: "Spiritual Journey", 
+    youtubeUrl: "https://youtu.be/umDFjJjh0_c",
+  },
+  {
+    id: "wolf-fang",
+    slug: "wolf-fang",
+    name: "Wolf Fang",
+    image: "/relics/wolf-fang.jpg",
+    songTitle: "I'm on fire",
+    youtubeUrl: "https://youtu.be/8XQUhWB_N5M",
+  },
+  {
+    id: "thunder-drums", 
+    slug: "thunder-drums",
+    name: "Thunder Drums",
+    image: "/relics/thunder-drums.jpg",
+    songTitle: "Horn of War",
+    youtubeUrl: "https://youtu.be/M4wGCg5oCx0",
+  },
+  {
+    id: "spirit-mantle",
+    slug: "spirit-mantle",
+    name: "Spirit Mantle",
+    image: "/relics/spirit-mantle.jpg", 
+    songTitle: "Iron Collide",
+    youtubeUrl: "https://youtu.be/fIkUDO2emoc",
+  },
+  {
+    id: "chronicle-stone",
+    slug: "chronicle-stone",
+    name: "Chronicle Stone", 
+    image: "/relics/chronicle-stone.jpg",
+    songTitle: "Heaven Calling",
+    youtubeUrl: "https://youtu.be/oxNauKuxg4Q",
+  },
+  {
+    id: "runningwolfs-blade",
+    slug: "runningwolfs-blade",
+    name: "Runningwolf's Blade",
+    image: "/relics/runningwolfs-blade.jpg",
+    songTitle: "Blood of Cross", // GUESSING - you still owe me this 8th old song title
+    youtubeUrl: "", // Need the link too
+  },
+];
