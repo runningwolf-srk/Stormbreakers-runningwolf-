470d963e2ed45912baaf6eb75bb21c9e1c0dc40f
runningwolf-srk <runningwolf1987@gmail.com>
2026-07-06 13:07:40 -0500

    Refactor armory.ts to use RelicPage component
    Refactored armory.ts to implement RelicPage component and removed ARMORY_RELICS array.

--- data/[id]/armory.ts
+++ data/[id]/armory.ts
@@ -1,80 +1,15 @@
-export type ArmoryRelic = {
-  id: string; // This matches your [id] folder
-  slug: string;
-  name: string;
-  image: string;
-  songTitle: string;
-  youtubeUrl: string;
-};
+import { getRelicById } from '@/data/[id]/armory';
 
-export const ARMORY_RELICS: ArmoryRelic[] = [
-  {
-    id: "storm-crown", // use slug as id for clean URLs
-    slug: "storm-crown",
-    name: "Storm Crown", 
-    image: "/relics/storm-crown.jpg",
-    songTitle: "Jesus is an healer",
-    youtubeUrl: "https://youtu.be/stsryByK5h8",
-  },
-  {
-    id: "eternal-fire",
-    slug: "eternal-fire",
-    name: "Eternal Fire",
-    image: "/relics/eternal-fire.jpg", 
-    songTitle: "Lord of Lords",
-    youtubeUrl: "https://youtu.be/9vP0NPrEv9s",
-  },
-  {
-    id: "first-flame",
-    slug: "first-flame",
-    name: "The First Flame",
-    image: "/relics/first-flame.jpg",
-    songTitle: "Spiritual Journey", 
-    youtubeUrl: "https://youtu.be/umDFjJjh0_c",
-  },
-  {
-    id: "wolf-fang",
-    slug: "wolf-fang",
-    name: "Wolf Fang",
-    image: "/relics/wolf-fang.jpg",
-    songTitle: "I'm on fire",
-    youtubeUrl: "https://youtu.be/8XQUhWB_N5M",
-  },
-  {
-    id: "thunder-drums", 
-    slug: "thunder-drums",
-    name: "Thunder Drums",
-    image: "/relics/thunder-drums.jpg",
-    songTitle: "Horn of War",
-    youtubeUrl: "https://youtu.be/M4wGCg5oCx0",
-  },
-  {
-    id: "spirit-mantle",
-    slug: "spirit-mantle",
-    name: "Spirit Mantle",
-    image: "/relics/spirit-mantle.jpg", 
-    songTitle: "Iron Collide",
-    youtubeUrl: "https://youtu.be/fIkUDO2emoc",
-  },
-  {
-    id: "chronicle-stone",
-    slug: "chronicle-stone",
-    name: "Chronicle Stone", 
-    image: "/relics/chronicle-stone.jpg",
-    songTitle: "Heaven Calling",
-    youtubeUrl: "https://youtu.be/oxNauKuxg4Q",
-  },
-  {
-    id: "runningwolfs-blade",
-    slug: "runningwolfs-blade",
-    name: "Runningwolf's Blade",
-    image: "/relics/runningwolfs-blade.jpg",
-    songTitle: "TBD - Need 8th old song", // Still missing this one
-    youtubeUrl: "",
-  },
-];
-
-// Helper for /app/armory/[id]/page.tsx
-export function getRelicById(id: string) {
-  return ARMORY_RELICS.find(relic => relic.id === id);
+export default function RelicPage({ params }: { params: { id: string } }) {
+  const relic = getRelicById(params.id);
+  
+  if (!relic) return <div>Relic not found</div>;
+  
+  return (
+    <div key={relic.id}>
+      <h1>{relic.name}</h1>
+      <img src={relic.image} alt={relic.name} />
+      <a href={relic.youtubeUrl}>{relic.songTitle}</a>
+    </div>
+  );
 }
