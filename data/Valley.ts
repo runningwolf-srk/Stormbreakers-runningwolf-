4a0e2a0963bb245667f899459fc347d1efb1369b
runningwolf-srk <runningwolf1987@gmail.com>
2026-07-06 12:56:34 -0500

    Add ValleyTrack type and VALLEY_TRACKS data
    Define ValleyTrack type and export VALLEY_TRACKS array with track details.

---
+++ data/Valley.ts
@@ -0,0 +1,16 @@
+export type ValleyTrack = {
+  title: string;
+  youtubeUrl: string;
+  relicOrigin?: string; // what relic it was originally for
+};
+
+export const VALLEY_TRACKS: ValleyTrack[] = [
+  { title: "The First Flame", youtubeUrl: "https://youtube.com/shorts/4IXJjvo7Ojo", relicOrigin: "The First Flame" },
+  { title: "Eternal Fire", youtubeUrl: "https://youtu.be/maI6giUcYYk", relicOrigin: "Eternal Fire" },
+  { title: "Runningwolf's Blade", youtubeUrl: "https://youtu.be/CZ6tylWuerQ", relicOrigin: "Runningwolf's Blade" },
+  { title: "Storm Crown", youtubeUrl: "https://youtu.be/MWHp_CRrKC8", relicOrigin: "Storm Crown" },
+  { title: "Chronicle Stone", youtubeUrl: "https://youtu.be/YQZS2X1GZHE", relicOrigin: "Chronicle Stone" },
+  { title: "Spirit Mantle", youtubeUrl: "https://youtu.be/LSmQtFkHGjE", relicOrigin: "Spirit Mantle" },
+  { title: "Mahonnath Deva", youtubeUrl: "https://youtu.be/BCKoGjYJZdg", relicOrigin: "Thunder Drums" },
+  { title: "Wolf Fang", youtubeUrl: "https://youtu.be/dHz0D3QblpQ", relicOrigin: "Wolf Fang" },
+];
