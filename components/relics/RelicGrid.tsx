6872685f196c79e511556db2b200b5f1007fb6d7
runningwolf-srk <runningwolf1987@gmail.com>
2026-07-07 21:59:45 -0500

    Refactor RelicGrid to export as a named function

--- components/relics/RelicGrid.tsx
+++ components/relics/RelicGrid.tsx
@@ -1,14 +1,14 @@
 // components/relics/RelicGrid.tsx
-import RelicCard from './RelicCard'
+import { RelicCard } from './RelicCard'
 import type { Relic } from '../../data/armory'
 
 type Props = {
   relics: Relic[]
 }
 
-export default function RelicGrid({ relics }: Props) {
+export function RelicGrid({ relics }: Props) {
   return (
-    <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
+    <div className="space-y-12">
       {relics.map((relic) => (
         <RelicCard key={relic.slug} relic={relic} />
       ))}
