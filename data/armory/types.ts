3769a58ed95a3aa63303236baa332cc8254fba28
runningwolf-srk <runningwolf1987@gmail.com>
2026-07-11 15:24:20 -0500

    Add Relic type definition in types.ts

---
+++ data/armory/types.ts
@@ -0,0 +1,15 @@
+export type Relic = {
+  slug: string
+  relic: string
+  song: string
+  scripture: string
+  reference: string
+  youtube?: string
+  declaration: string
+  reflection: string
+  testimony?: string
+  subtitle?: string
+  primaryTheme: `Healing` | `Calling` | `Spiritual Warfare` | `Redemption` | `Worship` | `Kingship` | `Fire`
+  themes: string[]
+  image?: string
+}
