d47c3389c256e890ff67b36771dfff04c7fd97c5
runningwolf-srk <runningwolf1987@gmail.com>
2026-07-11 23:28:41 -0500

    Clean up index.ts by removing unused code
    Removed unused imports and the armory array initialization.

--- data/armory/index.ts
+++ data/armory/index.ts
@@ -1,12 +1,3 @@
-// data/armory/index.ts
-import { healingRelics } from './healing'
-import { callingRelics } from './calling'
-import { warfareRelics } from './warfare'
-import { fireRelics } from './fire'
-import { kingshipRelics } from './kingship'
-import { worshipRelics } from './worship'
-import { redemptionRelics } from './redemption'
-
 export type Relic = {
   slug: string
   relic: string
@@ -22,13 +13,3 @@ export type Relic = {
   themes: string[]
   image?: string
 }
-
-export const armory: Relic[] = [
-  ...healingRelics,
-  ...callingRelics,
-  ...warfareRelics,
-  ...fireRelics,
-  ...kingshipRelics,
-  ...worshipRelics,
-  ...redemptionRelics,
-];
