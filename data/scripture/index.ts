50b35bfb37dcf9957d45cec5acb305ad152dc582
runningwolf-srk <runningwolf1987@gmail.com>
2026-07-12 18:34:29 -0500

    Uncomment fire and worship scripture exports

--- data/scripture/index.ts
+++ data/scripture/index.ts
@@ -13,23 +13,23 @@ export * from './redemption'
 export * from './kingship'
 export * from './warfare'
 export * from './calling'
-// export * from './fire'    // ← comment out until file exists
-// export * from './worship' // ← comment out until file exists
+export * from './fire'
+export * from './worship'
 
 import { healingScriptures } from './healing'
 import { redemptionScriptures } from './redemption'
 import { kingshipScriptures } from './kingship'
 import { warfareScriptures } from './warfare'
 import { callingScriptures } from './calling'
-// import { fireScriptures } from './fire'       // ← comment out
-// import { worshipScriptures } from './worship' // ← comment out
+import { fireScriptures } from './fire'
+import { worshipScriptures } from './worship'
 
 export const allScriptures: Scripture[] = [
-  ...healingScriptures,
-  ...redemptionScriptures,
-  ...kingshipScriptures,
-  ...warfareScriptures,
-  ...callingScriptures,
-  // ...fireScriptures,    // ← comment out
-  // ...worshipScriptures, // ← comment out
+ ...healingScriptures,
+ ...redemptionScriptures,
+ ...kingshipScriptures,
+ ...warfareScriptures,
+ ...callingScriptures,
+ ...fireScriptures,
+ ...worshipScriptures,
 ];
