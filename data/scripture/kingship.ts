ef31d59b388dad9abd572964d22f34b3813941ed
runningwolf-srk <runningwolf1987@gmail.com>
2026-07-12 18:07:01 -0500

    Add kingship scriptures data structure

---
+++ data/scripture/kingship.ts
@@ -0,0 +1,14 @@
+// data/scripture/kingship.ts
+import { Scripture } from './index'
+
+export const kingshipScriptures: Scripture[] = [
+  {
+    slug: "1-peter-2-9",
+    reference: "1 Peter 2:9",
+    theme: "Kingship",
+    text: "But you are a chosen people, a royal priesthood, a holy nation...",
+    context: "Peter reminding believers of their identity in Christ.",
+    connectedRelics: ["royal-relic"],
+    connectedChapters: [4]
+  }
+];
