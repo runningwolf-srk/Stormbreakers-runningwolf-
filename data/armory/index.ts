358521460810ee2205f55873efe3ec528d4d5baf
runningwolf-srk <runningwolf1987@gmail.com>
2026-07-11 23:34:43 -0500

    Import and export various relic types in armory

--- data/armory/index.ts
+++ data/armory/index.ts
@@ -1,3 +1,12 @@
+// data/armory/index.ts
+import { healingRelics } from './healing'
+import { callingRelics } from './calling'
+import { warfareRelics } from './warfare'
+import { fireRelics } from './fire'
+import { kingshipRelics } from './kingship'
+import { worshipRelics } from './worship'
+import { redemptionRelics } from './redemption'
+
 export type Relic = {
   slug: string
   relic: string
@@ -13,3 +22,14 @@ export type Relic = {
   themes: string[]
   image?: string
 }
+
+// THIS LINE IS CRITICAL - it exports 'armory'
+export const armory: Relic[] = [
+  ...healingRelics,
+  ...callingRelics,
+  ...warfareRelics,
+  ...fireRelics,
+  ...kingshipRelics,
+  ...worshipRelics,
+  ...redemptionRelics,
+];
