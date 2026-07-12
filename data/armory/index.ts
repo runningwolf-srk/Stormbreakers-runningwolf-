9a4e536f6cfdead60d9b098caa36e40e5d719e8c
runningwolf-srk <runningwolf1987@gmail.com>
2026-07-12 18:23:13 -0500

    Refactor index.ts to streamline relic exports

--- data/armory/index.ts
+++ data/armory/index.ts
@@ -1,44 +1,22 @@
-// data/armory/index.ts
-import { callingRelics } from './calling'
-import { fireRelics } from './fire'
+export * from './redemption'
+export * from './kingship'
+export * from './warfare'
+export * from './fire'
+export * from './calling'
+
 import { redemptionRelics } from './redemption'
 import { kingshipRelics } from './kingship'
-import { worshipRelics } from './worship'
 import { warfareRelics } from './warfare'
+import { fireRelics } from './fire'
+import { callingRelics } from './calling'
 
-export interface Relic {
-  slug: string
-  relic: string
-  song: string
-  scripture: string
-  reference: string
-  primaryTheme: string
-  themes: string[]
-  declaration: string
-  reflection: string
-  testimony: string
-  subtitle: string
-  youtube?: string
-  image: string
-}
-
-export const allRelics: Relic[] = [
-  ...callingRelics,
-  ...fireRelics,
+export const allRelics = [
   ...redemptionRelics,
   ...kingshipRelics,
-  ...worshipRelics,
   ...warfareRelics,
-]
+  ...fireRelics,
+  ...callingRelics,
+];
 
-export const armory = allRelics // ← This line fixes your build error
-
-export const getRelicBySlug = (slug: string): Relic | undefined => {
-  return allRelics.find(relic => relic.slug === slug)
-}
-
-export const getRelicsByTheme = (theme: string): Relic[] => {
-  return allRelics.filter(relic => 
-    relic.primaryTheme === theme || relic.themes.includes(theme)
-  )
-}
+export const armory = allRelics; // ← The Word imports this
+export { Relic } from './types'; // if you have a types file
