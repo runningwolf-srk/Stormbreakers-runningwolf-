5c730bcba9c6cf1657f9e5bdf018d35e67c4e82d
runningwolf-srk <runningwolf1987@gmail.com>
2026-07-12 12:50:53 -0500

    Refactor Relic interface and consolidate relic exports
    Updated the Relic interface and modified the export to include all relics. Added missing imports and adjusted the structure of the allRelics array.

--- data/armory/index.ts
+++ data/armory/index.ts
@@ -1,35 +1,36 @@
 // data/armory/index.ts
-import { healingRelics } from './healing'
 import { callingRelics } from './calling'
-import { warfareRelics } from './warfare'
 import { fireRelics } from './fire'
-import { kingshipRelics } from './kingship'
-import { worshipRelics } from './worship'
 import { redemptionRelics } from './redemption'
+import { kingshipRelics from './kingship'
+import { worshipRelics } from './worship'
+import { warfareRelics } from './warfare' // ← Add this import
 
-export type Relic = {
+export interface Relic {
   slug: string
   relic: string
   song: string
   scripture: string
   reference: string
-  youtube?: string
+  primaryTheme: string
+  themes: string[]
   declaration: string
   reflection: string
-  testimony?: string
-  subtitle?: string
-  primaryTheme: "Healing" | "Calling" | "Spiritual Warfare" | "Redemption" | "Worship" | "Kingship" | "Fire"
-  themes: string[]
-  image?: string
+  testimony: string
+  subtitle: string
+  youtube?: string // ← Make sure this is optional
+  image: string
 }
 
-// THIS LINE IS CRITICAL - it exports 'armory'
-export const armory: Relic[] = [
-  ...healingRelics,
+export const allRelics: Relic[] = [
   ...callingRelics,
-  ...warfareRelics,
   ...fireRelics,
+  ...redemptionRelics,
   ...kingshipRelics,
   ...worshipRelics,
-  ...redemptionRelics,
-];
+  ...warfareRelics, // ← Add this spread
+]
+
+export const getRelicBySlug = (slug: string): Relic | undefined => {
+  return allRelics.find(relic => relic.slug === slug)
+}
