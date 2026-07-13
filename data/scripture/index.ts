fac2aef21a5c8ec6c36a0a005f27d2b47788519b
runningwolf-srk <runningwolf1987@gmail.com>
2026-07-12 19:56:14 -0500

    Refactor Scripture interface and clean up imports

--- data/scripture/index.ts
+++ data/scripture/index.ts
@@ -7,8 +7,8 @@ export interface Scripture {
   theme: string
   text: string
   context: string
-  relatedRelics?: string[] // <- add this line
-  relatedChapters?: number[] // <- add this line
+  relatedRelics?: string[]
+  relatedChapters?: number[]
 }
 
 // Import all theme files
@@ -17,16 +17,14 @@ import { redemptionScriptures } from './redemption'
 import { kingshipScriptures } from './kingship'
 import { callingScriptures } from './calling'
 import { fireScriptures } from './fire'
-// import { worshipScriptures } from './worship' // add when ready
-// import { warfareScriptures } from './warfare' // add when ready
 
 // Combine into one array for The Word page
 export const allScriptures: Scripture[] = [
- ...healingScriptures,
- ...redemptionScriptures,
- ...kingshipScriptures,
- ...callingScriptures,
- ...fireScriptures,
+  ...healingScriptures,
+  ...redemptionScriptures,
+  ...kingshipScriptures,
+  ...callingScriptures,
+  ...fireScriptures,
 ]
 
 export default allScriptures
