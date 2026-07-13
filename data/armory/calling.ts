e7c1fbe0a612d0ed3931310a279617a59294858d
runningwolf-srk <runningwolf1987@gmail.com>
2026-07-13 10:19:26 -0500

    Remove 'Thunder Drums' entry from calling.ts
    Removed the 'Thunder Drums' entry as it has been moved to warfare.ts.

--- data/armory/calling.ts
+++ data/armory/calling.ts
@@ -11,15 +11,6 @@ export const callingRelics: Relic[] = [
     scriptureRef: 'Ephesians 6:17',
     lyrics: 'Sword of the Spirit - RunningWolf',
     youtube: 'https://www.youtube.com/watch?v=REPLACE_ME'
-  },
-  {
-    slug: 'thunder-drums',
-    title: 'Thunder Drums',
-    image: '/relics/thunder-drums.jpg',
-    theme: 'The Sound of the Storm',
-    description: "I march to the sound of heaven's drums. The storm is my cover. The thunder is His voice. I advance, not retreat.",
-    scriptureRef: 'Psalm 29:3-4',
-    lyrics: 'Thunder - Stormbreakers',
-    youtube: 'https://www.youtube.com/watch?v=REPLACE_ME'
   }
+  // DELETE thunder-drums from this file - it lives in warfare.ts now
 ]
