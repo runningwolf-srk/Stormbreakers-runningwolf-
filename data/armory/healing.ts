fc770564a73d207263d0ee0dc79fd8a951faa696
runningwolf-srk <runningwolf1987@gmail.com>
2026-07-13 10:20:31 -0500

    Remove Psalm 23 relic from healing.ts
    Removed Psalm 23 relic details as it has been moved to worship.ts.

--- data/armory/healing.ts
+++ data/armory/healing.ts
@@ -3,16 +3,6 @@ import { Relic } from './index'
 
 export const healingRelics: Relic[] = [
   {
-    slug: 'psalm-23',
-    title: 'Psalm 23',
-    image: '/relics/psalm-23.jpg',
-    theme: 'The Shepherd King',
-    description: 'I shall not want. I fear no evil. My cup overflows.',
-    scriptureRef: 'Psalm 23:1-6',
-    lyrics: 'Psalm 23 - Shane & Shane',
-    youtube: 'https://www.youtube.com/watch?v=REPLACE_ME'
-  },
-  {
     slug: 'im-on-fire',
     title: "I'm On Fire",
     image: '/relics/im-on-fire.jpg',
@@ -22,4 +12,5 @@ export const healingRelics: Relic[] = [
     lyrics: "I'm On Fire - RunningWolf",
     youtube: 'https://www.youtube.com/watch?v=REPLACE_ME'
   }
+  // DELETE psalm-23 from this file - it lives in worship.ts now
 ]
