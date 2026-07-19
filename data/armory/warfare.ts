0fc73c158fcfb7e1669ca898da0a6c5a6f87a215
runningwolf-srk <runningwolf1987@gmail.com>
2026-07-19 18:10:15 -0500

    Update warfareRelics with new properties and values

--- data/armory/warfare.ts
+++ data/armory/warfare.ts
@@ -1,4 +1,2 @@
 import { Relic } from './types'
-export const warfareRelics: Relic[] = [
-  { slug: 'thunder-drums', title: 'Thunder Drums', image: '/relics/thunder-drums.jpg', theme: 'Sound of the Storm', description: 'God of glory thunders.', scriptureRef: 'Psalm 29:3', lyrics: 'Thunder Drums', youtube: 'https://youtube.com', symbol: 'thunder-drums' },
-]
+export const warfareRelics: Relic[] = [{ slug:'thunder-drums', title:'Thunder Drums', image:'/relics/thunder.jpg', theme:'Warfare', description:'Thunders.', scriptureRef:'Psalm 29:3', lyrics:'Thunder', youtube:'https://youtube.com', symbol:'thunder-drums', act:3 }]
