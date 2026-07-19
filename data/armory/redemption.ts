c9f4793a3b03eb6df4076a255c1c1f8e4f497dce
runningwolf-srk <runningwolf1987@gmail.com>
2026-07-19 14:52:40 -0500

    Simplify redemption relic properties
    Removed redundant fields from the redemption relic object.

--- data/armory/redemption.ts
+++ data/armory/redemption.ts
@@ -1,24 +1,13 @@
-// data/armory/redemption.ts
 import { Relic } from './types'
-
 export const redemptionRelics: Relic[] = [
   {
     slug: 'redeemed',
     title: 'Redeemed',
-    relic: 'Redeemed',
-    song: 'Redeemed',
-    songTitle: 'Redeemed - Big Daddy Weave Cover',
-    lyrics: 'Redeemed - Big Daddy Weave Cover',
     image: '/relics/redeemed.jpg',
     theme: 'The Exchange',
-    primaryTheme: 'Redemption',
-    themes: ['Redemption', 'Grace'],
-    description: 'I am not who I was. I am redeemed by the blood of the Lamb.',
-    declaration: 'I am redeemed, forgiven, restored.',
-    reflection: 'Ephesians 1:7 - Redemption through His blood.',
-    scripture: 'In him we have redemption through his blood.',
+    description: 'I am not who I was. I am redeemed.',
     scriptureRef: 'Ephesians 1:7',
-    reference: 'Ephesians 1:7 ESV',
+    lyrics: 'Redeemed - Big Daddy Weave Cover',
     youtube: 'https://www.youtube.com/watch?v=',
   },
 ]
