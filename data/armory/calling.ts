dff88f1e4a9a9e4c80db3ce3ed78387a62f37312
runningwolf-srk <runningwolf1987@gmail.com>
2026-07-19 15:15:21 -0500

    Refactor callingRelics array with new structure
    Removed old relic entries and consolidated into two entries with additional properties.

--- data/armory/calling.ts
+++ data/armory/calling.ts
@@ -1,23 +1,5 @@
 import { Relic } from './types'
 export const callingRelics: Relic[] = [
-  {
-    slug: 'running-wolf-blade',
-    title: 'Running Wolf Blade',
-    image: '/relics/wolf-blade.jpg',
-    theme: 'The Sword of the Sent One',
-    description: 'I am not just sent - I am armed. The Sword of the Spirit.',
-    scriptureRef: 'Ephesians 6:17',
-    lyrics: 'Sword of the Spirit - RunningWolf',
-    youtube: 'https://www.youtube.com/watch?v=',
-  },
-  {
-    slug: 'wolf-fang',
-    title: 'Wolf Fang',
-    image: '/relics/wolf-fang.jpg',
-    theme: 'The Tooth of the Sent One',
-    description: 'The tooth of the sent one. Hard as flint. The mark of RunningWolf.',
-    scriptureRef: 'Ezekiel 3:9',
-    lyrics: 'Wolf Fang - RunningWolf',
-    youtube: 'https://youtu.be/dHz0D3QblpQ',
-  },
+  { slug: 'wolf-fang', title: 'Wolf Fang', image: '/relics/wolf-fang.jpg', theme: 'Tooth of the Sent One', description: 'Hard as flint.', scriptureRef: 'Ezekiel 3:9', lyrics: 'Wolf Fang', youtube: 'https://youtu.be/dHz0D3QblpQ', symbol: 'wolf-fang' },
+  { slug: 'running-wolf-blade', title: 'Running Wolf Blade', image: '/relics/wolf-blade.jpg', theme: 'Sword of the Sent One', description: 'Armed with the Word.', scriptureRef: 'Ephesians 6:17', lyrics: 'Sword of the Spirit', youtube: 'https://youtube.com', symbol: 'running-wolf-blade' },
 ]
