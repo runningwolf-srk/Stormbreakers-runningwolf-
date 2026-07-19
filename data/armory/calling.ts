62631003be19cd3b6f02faf3797413ffc36b2e0a
runningwolf-srk <runningwolf1987@gmail.com>
2026-07-19 18:09:30 -0500

    Update callingRelics with new themes and properties

--- data/armory/calling.ts
+++ data/armory/calling.ts
@@ -1,5 +1,5 @@
 import { Relic } from './types'
 export const callingRelics: Relic[] = [
-  { slug: 'wolf-fang', title: 'Wolf Fang', image: '/relics/wolf-fang.jpg', theme: 'Tooth of the Sent One', description: 'Hard as flint.', scriptureRef: 'Ezekiel 3:9', lyrics: 'Wolf Fang', youtube: 'https://youtu.be/dHz0D3QblpQ', symbol: 'wolf-fang' },
-  { slug: 'running-wolf-blade', title: 'Running Wolf Blade', image: '/relics/wolf-blade.jpg', theme: 'Sword of the Sent One', description: 'Armed with the Word.', scriptureRef: 'Ephesians 6:17', lyrics: 'Sword of the Spirit', youtube: 'https://youtube.com', symbol: 'running-wolf-blade' },
+{ slug:'wolf-fang', title:'Wolf Fang', image:'/relics/wolf-fang.jpg', theme:'Identity', description:'Hard as flint.', scriptureRef:'Ezekiel 3:9', lyrics:'Fang', youtube:'https://youtu.be/dHz0D3QblpQ', symbol:'wolf-fang', act:3 },
+{ slug:'running-wolf-blade', title:'Wolf Blade', image:'/relics/blade.jpg', theme:'Warfare', description:'Sword.', scriptureRef:'Ephesians 6:17', lyrics:'Blade', youtube:'https://youtube.com', symbol:'blade', act:3 }
 ]
