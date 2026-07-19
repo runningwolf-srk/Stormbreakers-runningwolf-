e2f8a673f5418a88a168700c0a40344e136e3996
runningwolf-srk <runningwolf1987@gmail.com>
2026-07-19 15:19:13 -0500

    Refactor worshipRelics for psalm-23
    Updated worshipRelics array to include a new property 'symbol' and modified existing properties for 'psalm-23'.

--- data/armory/worship.ts
+++ data/armory/worship.ts
@@ -1,13 +1,4 @@
 import { Relic } from './types'
 export const worshipRelics: Relic[] = [
-  {
-    slug: 'psalm-23',
-    title: 'Psalm 23',
-    image: '/relics/psalm-23.jpg',
-    theme: 'The Shepherd King',
-    description: 'The Lord is my shepherd; I shall not want.',
-    scriptureRef: 'Psalm 23:1-3',
-    lyrics: 'Psalm 23',
-    youtube: 'https://www.youtube.com/watch?v=',
-  },
+  { slug: 'psalm-23', title: 'Psalm 23', image: '/relics/psalm-23.jpg', theme: 'Shepherd King', description: 'The Lord is my shepherd.', scriptureRef: 'Psalm 23:1', lyrics: 'Psalm 23', youtube: 'https://youtube.com', symbol: 'psalm-23' },
 ]
