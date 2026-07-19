6fd6800e0b98a70487eb54fe60acf0e27eba947f
runningwolf-srk <runningwolf1987@gmail.com>
2026-07-19 15:13:34 -0500

    Refactor redemption relic properties
    Updated the redemption relic to include a symbol and modified the lyrics and YouTube link.

--- data/armory/redemption.ts
+++ data/armory/redemption.ts
@@ -1,13 +1,4 @@
 import { Relic } from './types'
 export const redemptionRelics: Relic[] = [
-  {
-    slug: 'redeemed',
-    title: 'Redeemed',
-    image: '/relics/redeemed.jpg',
-    theme: 'The Exchange',
-    description: 'I am not who I was. I am redeemed.',
-    scriptureRef: 'Ephesians 1:7',
-    lyrics: 'Redeemed - Big Daddy Weave Cover',
-    youtube: 'https://www.youtube.com/watch?v=',
-  },
+  { slug: 'redeemed', title: 'Redeemed', image: '/relics/redeemed.jpg', theme: 'The Exchange', description: 'I am redeemed.', scriptureRef: 'Ephesians 1:7', lyrics: 'Redeemed', youtube: 'https://youtube.com', symbol: 'redeemed' },
 ]
