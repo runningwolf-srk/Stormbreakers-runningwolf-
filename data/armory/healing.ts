f88ebafae6d9780cd43085bfaebeae32a44944c9
runningwolf-srk <runningwolf1987@gmail.com>
2026-07-19 18:08:11 -0500

    Modify healingRelics with new theme and properties
    Updated the healing relics array with new properties and values.

--- data/armory/healing.ts
+++ data/armory/healing.ts
@@ -1,4 +1,2 @@
 import { Relic } from './types'
-export const healingRelics: Relic[] = [
-  { slug: 'im-on-fire', title: "I'm On Fire", image: '/relics/im-on-fire.jpg', theme: 'Scars That Preach', description: 'Walked through fire and did not burn.', scriptureRef: 'Isaiah 43:2', lyrics: "I'm On Fire", youtube: 'https://youtube.com', symbol: 'im-on-fire' },
-]
+export const healingRelics: Relic[] = [{ slug:'im-on-fire', title:"I'm On Fire", image:'/relics/im-on-fire.jpg', theme:'Healing', description:'Healed.', scriptureRef:'Isaiah 43:2', lyrics:'Fire', youtube:'https://youtube.com', symbol:'im-on-fire', act:2 }]
