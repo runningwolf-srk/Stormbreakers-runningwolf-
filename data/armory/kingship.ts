1c644e6e8443aaca5bac1e767aa401a5f28836ae
runningwolf-srk <runningwolf1987@gmail.com>
2026-07-19 18:06:33 -0500

    Refactor kingshipRelics with new structure and values
    Updated the kingshipRelics array with new properties and modified existing values.

--- data/armory/kingship.ts
+++ data/armory/kingship.ts
@@ -1,5 +1,2 @@
 import { Relic } from './types'
-export const kingshipRelics: Relic[] = [
-  { slug: 'running-wolf', title: 'Running Wolf', image: '/relics/running-wolf.jpg', theme: 'The calling anthem', description: 'I am called, chosen, and sent.', scriptureRef: 'Isaiah 6:8', lyrics: 'Running Wolf', youtube: 'https://youtube.com', symbol: 'running-wolf' },
-  { slug: 'royal-relic', title: 'Royal Relic', image: '/relics/royal-relic.jpg', theme: 'The Crown', description: 'Royalty by the blood of the Lamb.', scriptureRef: '1 Peter 2:9', lyrics: 'Royal Relic', youtube: 'https://youtube.com', symbol: 'royal-relic' },
-]
+export const kingshipRelics: Relic[] = [{ slug:'running-wolf', title:'Running Wolf', image:'/relics/running-wolf.jpg', theme:'Calling', description:'Called and sent.', scriptureRef:'Isaiah 6:8', lyrics:'Running', youtube:'https://youtube.com', symbol:'running-wolf', act:1 }]
