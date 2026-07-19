95c322548e3229aa332161544e83b012eb95f719
runningwolf-srk <runningwolf1987@gmail.com>
2026-07-19 18:29:41 -0500

    Update next.config.js to modify image settings
    Removed remotePatterns configuration for images and added typescript ignoreBuildErrors option.

--- next.config.js
+++ next.config.js
@@ -1,13 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**',
-      },
-    ],
+  typescript: {
+    ignoreBuildErrors: true,
   },
 }
-
 module.exports = nextConfig
