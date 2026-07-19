368009c25cfb5f473b5525201e622ae873197bb0
runningwolf-srk <runningwolf1987@gmail.com>
2026-07-19 14:53:38 -0500

    Add fire relics with detailed information
    Added fire relics with details including titles, images, themes, descriptions, scripture references, lyrics, and YouTube links.

--- data/armory/fire.ts
+++ data/armory/fire.ts
@@ -1,2 +1,23 @@
 import { Relic } from './types'
-export const fireRelics: Relic[] = []
+export const fireRelics: Relic[] = [
+  {
+    slug: 'eternal-fire',
+    title: 'Eternal Fire',
+    image: '/relics/eternal-fire.jpg',
+    theme: 'The Fire That Never Goes Out',
+    description: 'The fire that refined me is the fire that defines me.',
+    scriptureRef: 'Leviticus 6:13',
+    lyrics: 'Eternal Fire - Stormbreakers',
+    youtube: 'https://www.youtube.com/watch?v=',
+  },
+  {
+    slug: 'spirit-mantle',
+    title: 'Spirit Mantle',
+    image: '/relics/spirit-mantle.jpg',
+    theme: "The Anointing You Can't Take Off",
+    description: "The anointing you can't take off. The Spirit wears you.",
+    scriptureRef: '2 Kings 2:9',
+    lyrics: 'Mantle - RunningWolf',
+    youtube: 'https://www.youtube.com/watch?v=',
+  },
+]
