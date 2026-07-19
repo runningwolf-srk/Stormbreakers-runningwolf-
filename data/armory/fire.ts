31c68bb5f30db34e274a13d44a30bbc1e3181a94
runningwolf-srk <runningwolf1987@gmail.com>
2026-07-19 14:49:02 -0500

    Clear fireRelics array and update import statement
    Removed existing fire relics and changed import path.

--- data/armory/fire.ts
+++ data/armory/fire.ts
@@ -1,25 +1,2 @@
-// data/armory/fire.ts
-import { Relic } from './index'
-
-export const fireRelics: Relic[] = [
-  {
-    slug: 'eternal-fire',
-    title: 'Eternal Fire',
-    image: '/relics/eternal-fire.jpg',
-    theme: 'The Fire That Never Goes Out',
-    description: 'The fire that refined me in ICU never goes out. It burns on the altar of my life. Consuming. Eternal. Holy.',
-    scriptureRef: 'Leviticus 6:13',
-    lyrics: 'Eternal Fire - Stormbreakers',
-    youtube: 'https://www.youtube.com/watch?v=REPLACE_ME'
-  },
-  {
-    slug: 'spirit-mantle',
-    title: 'Spirit Mantle',
-    image: '/relics/spirit-mantle.jpg',
-    theme: "The Anointing You Can't Take Off",
-    description: "The anointing you can't take off. The Spirit wears you. Fire from heaven, not from earth.",
-    scriptureRef: '2 Kings 2:9',
-    lyrics: 'Mantle - RunningWolf',
-    youtube: 'https://www.youtube.com/watch?v=REPLACE_ME'
-  }
-]
+import { Relic } from './types'
+export const fireRelics: Relic[] = []
