4446ffb833c64fbd8ad5e00661244d6188d5b442
runningwolf-srk <runningwolf1987@gmail.com>
2026-07-19 15:12:43 -0500

    Refactor kingshipRelics structure and content
    Updated relic entries with new properties and simplified descriptions.

--- data/armory/kingship.ts
+++ data/armory/kingship.ts
@@ -1,23 +1,5 @@
 import { Relic } from './types'
 export const kingshipRelics: Relic[] = [
-  {
-    slug: 'running-wolf',
-    title: 'Running Wolf',
-    image: '/relics/running-wolf.jpg',
-    theme: 'The calling anthem',
-    description: 'I am called, chosen, and sent.',
-    scriptureRef: 'Isaiah 6:8',
-    lyrics: 'Running Wolf - The Calling Anthem',
-    youtube: 'https://www.youtube.com/watch?v=',
-  },
-  {
-    slug: 'royal-relic',
-    title: 'Royal Relic',
-    image: '/relics/royal-relic.jpg',
-    theme: 'The Crown of the Chosen',
-    description: 'I am royalty. Not by bloodline, but by the blood of the Lamb.',
-    scriptureRef: '1 Peter 2:9',
-    lyrics: 'Royal Relic',
-    youtube: 'https://www.youtube.com/watch?v=',
-  },
+  { slug: 'running-wolf', title: 'Running Wolf', image: '/relics/running-wolf.jpg', theme: 'The calling anthem', description: 'I am called, chosen, and sent.', scriptureRef: 'Isaiah 6:8', lyrics: 'Running Wolf', youtube: 'https://youtube.com', symbol: 'running-wolf' },
+  { slug: 'royal-relic', title: 'Royal Relic', image: '/relics/royal-relic.jpg', theme: 'The Crown', description: 'Royalty by the blood of the Lamb.', scriptureRef: '1 Peter 2:9', lyrics: 'Royal Relic', youtube: 'https://youtube.com', symbol: 'royal-relic' },
 ]
