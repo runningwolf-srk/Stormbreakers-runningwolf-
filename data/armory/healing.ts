5377e377f735f986b06f3184142056ebba38a51a
runningwolf-srk <runningwolf1987@gmail.com>
2026-07-19 15:14:09 -0500

    Refactor healing relic entry for 'I'm On Fire'
    Condensed healing relic object properties into a single line and updated the description and YouTube link.

--- data/armory/healing.ts
+++ data/armory/healing.ts
@@ -1,13 +1,4 @@
 import { Relic } from './types'
 export const healingRelics: Relic[] = [
-  {
-    slug: 'im-on-fire',
-    title: "I'm On Fire",
-    image: '/relics/im-on-fire.jpg',
-    theme: 'Scars That Preach - Born Through Fire',
-    description: 'I walked through the fire and did not burn.',
-    scriptureRef: 'Isaiah 43:2',
-    lyrics: "I'm On Fire - RunningWolf",
-    youtube: 'https://www.youtube.com/watch?v=',
-  },
+  { slug: 'im-on-fire', title: "I'm On Fire", image: '/relics/im-on-fire.jpg', theme: 'Scars That Preach', description: 'Walked through fire and did not burn.', scriptureRef: 'Isaiah 43:2', lyrics: "I'm On Fire", youtube: 'https://youtube.com', symbol: 'im-on-fire' },
 ]
