4ac20d1628bea1c7c30e42edc1def786f91b07dc
runningwolf-srk <runningwolf1987@gmail.com>
2026-07-19 18:07:26 -0500

    Modify redemptionRelics with new properties
    Updated the redemptionRelics array to include an 'act' property and changed the theme description.

--- data/armory/redemption.ts
+++ data/armory/redemption.ts
@@ -1,4 +1,2 @@
 import { Relic } from './types'
-export const redemptionRelics: Relic[] = [
-  { slug: 'redeemed', title: 'Redeemed', image: '/relics/redeemed.jpg', theme: 'The Exchange', description: 'I am redeemed.', scriptureRef: 'Ephesians 1:7', lyrics: 'Redeemed', youtube: 'https://youtube.com', symbol: 'redeemed' },
-]
+export const redemptionRelics: Relic[] = [{ slug:'redeemed', title:'Redeemed', image:'/relics/redeemed.jpg', theme:'Redemption', description:'Redeemed.', scriptureRef:'Ephesians 1:7', lyrics:'Redeemed', youtube:'https://youtube.com', symbol:'redeemed', act:1 }]
