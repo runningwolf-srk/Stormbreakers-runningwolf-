6c8301cd1afe6d49f15f45b96e5e145778b89d5c
runningwolf-srk <runningwolf1987@gmail.com>
2026-07-19 14:53:07 -0500

    Simplify healing relic properties
    Removed redundant properties from healing relic.

--- data/armory/healing.ts
+++ data/armory/healing.ts
@@ -1,24 +1,13 @@
-// data/armory/healing.ts
 import { Relic } from './types'
-
 export const healingRelics: Relic[] = [
   {
     slug: 'im-on-fire',
     title: "I'm On Fire",
-    relic: "I'm On Fire",
-    song: "I'm On Fire",
-    songTitle: "I'm On Fire - RunningWolf",
-    lyrics: "I'm On Fire - RunningWolf",
     image: '/relics/im-on-fire.jpg',
     theme: 'Scars That Preach - Born Through Fire',
-    primaryTheme: 'Healing',
-    themes: ['Healing', 'Fire', 'Testimony'],
-    description: 'I walked through the fire and did not burn. The fourth man was with me.',
-    declaration: 'I am healed, I am on fire for Jesus.',
-    reflection: 'Isaiah 43:2 - When you walk through the fire you will not be burned.',
-    scripture: 'When you walk through the fire, you will not be burned.',
+    description: 'I walked through the fire and did not burn.',
     scriptureRef: 'Isaiah 43:2',
-    reference: 'Isaiah 43:2 ESV',
+    lyrics: "I'm On Fire - RunningWolf",
     youtube: 'https://www.youtube.com/watch?v=',
   },
 ]
