e0edf9c654849cc3aeab1542b2a4d07590430a8b
runningwolf-srk <runningwolf1987@gmail.com>
2026-07-19 14:54:39 -0500

    Refactor Thunder Drums relic properties
    Removed redundant fields and updated scripture reference for Thunder Drums relic.

--- data/armory/warfare.ts
+++ data/armory/warfare.ts
@@ -1,24 +1,13 @@
-// data/armory/warfare.ts
 import { Relic } from './types'
-
 export const warfareRelics: Relic[] = [
   {
     slug: 'thunder-drums',
     title: 'Thunder Drums',
-    relic: 'Thunder Drums',
-    song: 'Thunder Drums',
-    songTitle: 'Thunder Drums',
-    lyrics: 'Thunder Drums',
     image: '/relics/thunder-drums.jpg',
     theme: 'The Sound of the Storm',
-    primaryTheme: 'Warfare',
-    themes: ['Warfare', 'Worship', 'Authority'],
     description: 'I march to the sound of heaven. The God of glory thunders.',
-    declaration: 'The Lord thunders over His enemies.',
-    reflection: 'Psalm 29:3 - The God of glory thunders.',
-    scripture: 'The voice of the Lord is over the waters; the God of glory thunders.',
-    scriptureRef: 'Isaiah 42:13 NIV',
-    reference: 'Psalm 29:3 ESV',
+    scriptureRef: 'Isaiah 42:13',
+    lyrics: 'Thunder Drums',
     youtube: 'https://www.youtube.com/watch?v=',
   },
 ]
