85edcf6a8e16f60959eeb20a4a64f11197e91225
runningwolf-srk <runningwolf1987@gmail.com>
2026-07-19 18:10:44 -0500

    Modify worshipRelics with new attributes
    Updated the worshipRelics array with new properties and values.

--- data/armory/worship.ts
+++ data/armory/worship.ts
@@ -1,4 +1,2 @@
 import { Relic } from './types'
-export const worshipRelics: Relic[] = [
-  { slug: 'psalm-23', title: 'Psalm 23', image: '/relics/psalm-23.jpg', theme: 'Shepherd King', description: 'The Lord is my shepherd.', scriptureRef: 'Psalm 23:1', lyrics: 'Psalm 23', youtube: 'https://youtube.com', symbol: 'psalm-23' },
-]
+export const worshipRelics: Relic[] = [{ slug:'psalm-23', title:'Psalm 23', image:'/relics/psalm23.jpg', theme:'Worship', description:'My shepherd.', scriptureRef:'Psalm 23:1', lyrics:'Psalm 23', youtube:'https://youtube.com', symbol:'psalm-23', act:1 }]
