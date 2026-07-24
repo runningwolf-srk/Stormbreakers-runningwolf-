32cfe034a4bb018765036729506913a4af2b8d6f
runningwolf-srk <runningwolf1987@gmail.com>
2026-07-24 14:57:38 -0500

    Refactor CANON_PROGRESS export and clean up code

--- data/armory/index.ts
+++ data/armory/index.ts
@@ -2,14 +2,6 @@ import { callingRelics } from "./calling"
 import { warfareRelics } from "./warfare"
 import { worshipRelics } from "./worship"
 import { legacyRelics } from "./legacy"
-
 export const ALL_RELICS = [...callingRelics, ...warfareRelics, ...worshipRelics, ...legacyRelics].filter(Boolean)
-
-export const CANON_PROGRESS = {
-  forged: 2,
-  inProgress: 1,
-  unrevealed: 13,
-  total: 16
-}
-
+export const CANON_PROGRESS = { forged: 2, inProgress: 1, unrevealed: 13, total: 16 }
 export { callingRelics, warfareRelics, worshipRelics, legacyRelics }
