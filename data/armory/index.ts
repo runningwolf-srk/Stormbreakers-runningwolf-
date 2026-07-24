79934b0ca03376bc23ebedfa0cee012ee788990c
runningwolf-srk <runningwolf1987@gmail.com>
2026-07-24 15:38:57 -0500

    Refactor ALL_RELICS to use CANON_ORDER

--- data/armory/index.ts
+++ data/armory/index.ts
@@ -2,6 +2,10 @@ import { callingRelics } from "./calling"
 import { warfareRelics } from "./warfare"
 import { worshipRelics } from "./worship"
 import { legacyRelics } from "./legacy"
-export const ALL_RELICS = [...callingRelics, ...warfareRelics, ...worshipRelics, ...legacyRelics].filter(Boolean)
+export const ALL_RELICS_ORDERED = [...callingRelics, ...warfareRelics, ...worshipRelics, ...legacyRelics].filter(Boolean)
+  .sort((a,b)=> a.slug.localeCompare(b.slug)) // will be replaced by canon order below
+// Canon order: VII, X, XI
+export const CANON_ORDER = ["wolf-fang","wolf-blade","thunder-drums"]
+export const ALL_RELICS = CANON_ORDER.map(slug => [...callingRelics, ...warfareRelics].find(r=>r.slug===slug)!).filter(Boolean)
 export const CANON_PROGRESS = { forged: 2, inProgress: 1, unrevealed: 13, total: 16 }
 export { callingRelics, warfareRelics, worshipRelics, legacyRelics }
