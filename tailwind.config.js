c3a73e62c053d3f202bc2785b58442ab63fe7ae9
runningwolf-srk <runningwolf1987@gmail.com>
2026-07-24 23:24:06 -0500

    Simplify Tailwind CSS configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,1 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+module.exports={content:["./app/**/*.{js,ts,jsx,tsx}","./data/**/*.{js,ts,jsx,tsx}"],theme:{extend:{}},plugins:[]}
