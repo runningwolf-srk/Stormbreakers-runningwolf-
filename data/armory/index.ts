b53791888203564b27aacf34878401c551280b6e
runningwolf-srk <runningwolf1987@gmail.com>
2026-07-24 22:56:13 -0500

    Update CANON_PROGRESS values in index.ts

--- data/armory/index.ts
+++ data/armory/index.ts
@@ -2,5 +2,5 @@ import {callingRelics} from "./calling";import {warfareRelics} from "./warfare";
 export const CANON_ORDER=["wolf-fang","wolf-blade","thunder-drums"];
 export const CANON_LEDGER:Record<string,{num:string}>={"wolf-fang":{num:"VII"},"wolf-blade":{num:"X"},"thunder-drums":{num:"XI"}};
 export const ALL_RELICS=[...callingRelics,...warfareRelics,...worshipRelics,...legacyRelics].filter(Boolean).map(r=>({...r,num:CANON_LEDGER[r.slug]?.num||r.num})).filter(r=>CANON_ORDER.includes(r.slug)).sort((a,b)=>CANON_ORDER.indexOf(a.slug)-CANON_ORDER.indexOf(b.slug));
-export const CANON_PROGRESS={forged:2,inProgress:1,unrevealed:13,total:16};
+export const CANON_PROGRESS={forged:3,inProgress:0,unrevealed:13,total:16};
 export {callingRelics,warfareRelics,worshipRelics,legacyRelics};
